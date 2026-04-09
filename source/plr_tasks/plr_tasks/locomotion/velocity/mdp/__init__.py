5637ff5e64a9de33af989d191177d83fcf8830b6
Leon Auspurg <leonauspurg@bluewin.ch>
2026-04-09 18:49:02 +0200

    added necessary imports for observations and events

--- source/plr_tasks/plr_tasks/locomotion/velocity/mdp/__init__.py
+++ source/plr_tasks/plr_tasks/locomotion/velocity/mdp/__init__.py
@@ -10,3 +10,5 @@ from isaaclab.envs.mdp import *  # noqa: F401, F403
 from .curriculums import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
 from .terminations import *  # noqa: F401, F403
+from .observations import *
+from .events import *
