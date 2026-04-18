cc0c0bd62981bf1b3f5e0d618bf11af370bba539
Leon Auspurg <leonauspurg@bluewin.ch>
2026-04-18 09:48:43 +0200

    commented override rewards

--- source/plr_tasks/plr_tasks/locomotion/velocity/config/anymal_d/flat_env_cfg.py
+++ source/plr_tasks/plr_tasks/locomotion/velocity/config/anymal_d/flat_env_cfg.py
@@ -14,10 +14,10 @@ class AnymalDFlatEnvCfg(AnymalDRoughEnvCfg):
         # post init of parent
         super().__post_init__()
 
-        # override rewards
-        self.rewards.flat_orientation_l2.weight = -5.0
-        self.rewards.dof_torques_l2.weight = -2.5e-5
-        self.rewards.feet_air_time.weight = 0.5
+        # # override rewards
+        # self.rewards.flat_orientation_l2.weight = -5.0
+        # self.rewards.dof_torques_l2.weight = -2.5e-5
+        # self.rewards.feet_air_time.weight = 0.5
         # change terrain to flat
         self.scene.terrain.terrain_type = "plane"
         self.scene.terrain.terrain_generator = None
